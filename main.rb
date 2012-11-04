95b91e00f6d1eef47dfcaf63dd6f10381a02d17a
Basia Madej <basia@applicake.com>
2012-11-04 21:41:46 +0100

    photography basic page

--- main.rb
+++ main.rb
@@ -3,22 +3,43 @@ require 'sinatra'
 
 set :public_folder, 'public'
 
-get '/videos' do
+get '/1' do
   redirect '/1-videos/index.html'
 end
 
-get '/harry' do
+get '/2' do
   redirect '/2-harrypotter/index.html'
 end
 
-get '/donuts' do
+get '/3' do
   redirect '/3-donuts/index.html'
 end
 
-get '/homework' do
+get '/4' do
+  redirect '/4-photography/index.html'
+end
+
+get '/5' do
   redirect '/5-homework/index.html'
 end
 
-get '/photo' do
-  redirect '/4-photography/index.html'
+get '/1/old' do
+  redirect '/1-videos/mockup/index.html'
 end
+
+get '/2/old' do
+  redirect '/2-harrypotter/mockoup/index.html'
+end
+
+get '/3/old' do
+  redirect '/3-donuts/index.html'
+end
+
+get '/4/old' do
+  redirect '/4-photography/mockup/index.html'
+end
+
+get '/5/old' do
+  redirect '/5-homework/mockup/index.html'
+end
+
