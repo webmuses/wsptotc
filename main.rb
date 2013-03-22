03a72d9e6ec5e1877b89f2e920563ccba2f2c961
Basia Madej <basia@applicake.com>
2013-03-22 01:39:54 +0100

    removed mockoups

--- main.rb
+++ main.rb
@@ -19,31 +19,6 @@ get '/3' do
   redirect '/3-donuts/index.html'
 end
 
-get '/4' do
-  redirect '/4-photography/index.html'
-end
-
 get '/5' do
   redirect '/5-homework/index.html'
 end
-
-get '/1/old' do
-  redirect '/1-videos/mockup/index.html'
-end
-
-get '/2/old' do
-  redirect '/2-harrypotter/mockup/index.html'
-end
-
-get '/3/old' do
-  redirect '/3-donuts/mockup/index.html'
-end
-
-get '/4/old' do
-  redirect '/4-photography/mockup/index.html'
-end
-
-get '/5/old' do
-  redirect '/5-homework/mockup/index.html'
-end
-
